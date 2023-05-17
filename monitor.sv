be653be624be0875f43400ab1ecef5267335eb07
Powerhouse28 <26279434+Powerhouse28@users.noreply.github.com>
2023-05-16 18:28:27 -0700

    Update monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -19,22 +19,22 @@ endfunction
    trans_mon = new();
     @(posedge vif_fifo.clk);
 
-    if(`MONITOR_IF.wr_en) begin
+  //  if(`MONITOR_IF.wr_en) begin
     // trans_mon.wr_en = `MONITOR_IF.wr_en ;
      //trans_mon.data_in = `MONITOR_IF.data_in;
      trans_mon.full = `MONITOR_IF.full;
      trans_mon.empty = `MONITOR_IF.empty;
      $display("Write En: %h\tFIFO Output: %h",`MONITOR_IF.wr_en, `MONITOR_IF.data_in);
-     end
+  //   end
 
-    if(`MONITOR_IF.rd_en) begin
+ //   if(`MONITOR_IF.rd_en) begin
    //  trans_mon.rd_en = `MONITOR_IF.rd_en ;
      trans_mon.data_out = `MONITOR_IF.data_out;
-     trans_mon.full = `MONITOR_IF.full;
-     trans_mon.empty = `MONITOR_IF.empty;
+  //   trans_mon.full = `MONITOR_IF.full;
+  //   trans_mon.empty = `MONITOR_IF.empty;
 
 	$display("Read En: %h\tFIFO Output: %h",`MONITOR_IF.rd_en, `MONITOR_IF.data_out);
-  end 
+//  end 
   mon2scb.put(trans_mon);
  end  
  endtask
